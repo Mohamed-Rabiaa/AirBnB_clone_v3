c53711faf178a32d0224a9da3c326877be262260
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-05-24 04:09:13 -0700

    Starting my Api

---
+++ api/v1/views/index.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" index.py """
+
+
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status')
+def status():
+    """ Returns a JSON: "status": "OK" """
+    return jsonify({"status": "OK"})
