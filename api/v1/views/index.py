a42510ddfd832406973fc33f50af85ad35f2892c
Mohamed-Rabiaa <mohamedrabiaa1993@gmail.com>
2024-05-24 05:08:39 -0700

    Handling Route: /api/v1/stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,9 +4,27 @@
 
 from api.v1.views import app_views
 from flask import jsonify
-
+from models import storage
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 @app_views.route('/status')
 def status():
     """ Returns a JSON: "status": "OK" """
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def objs_number():
+    """ Retrieves the number of each objects by type """
+    dct = {}
+    dct['amenities'] = storage.count(Amenity)
+    dct['cities'] = storage.count(City)
+    dct['places'] = storage.count(Place)
+    dct['reviews'] = storage.count(Review)
+    dct['states'] = storage.count(State)
+    dct['users'] = storage.count(User)
+    return jsonify(dct)
